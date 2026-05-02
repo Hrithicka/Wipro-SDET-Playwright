054824baa2f8037721b32e0ecd43eb7db8f034b0
Hrithicka <hrithigiri03@gmail.com>
2026-05-02 11:30:56 +0530

    Array Methods

--- Day2/index.js
+++ Day2/index.js
@@ -2,17 +2,17 @@ let a = "Hrithicka";
 let b = "Agnes";
 let c = "Akshayaa"; 
 let d = "Hello " + a + ", " + b + " and " + c + "!"; // Concatenation
-console.log(d);
-// Using Template Literals
+// console.log(d);
+// Using Template literals
 let e = `Hello ${a}, ${b} and ${c}!`; // Template literals
 let f = `The length of a is ${a.length * 2}`; // Using template literals to get length
-console.log(e);
-console.log(f);
+// console.log(e);
+// console.log(f);
 
 function table(num) {
     let result = "";
     for (let i = 1; i <= 10; i++) {
-        console.log(num + " x " + i + " = " + (num * i)); // Using string concatenation
+        // console.log(num + " x " + i + " = " + (num * i)); // Using string concatenation
         result += `${num} x ${i} = ${num * i}\n`;
     }
     return result;
@@ -27,6 +27,7 @@ function table(num) {
 let sum = (x, y) => x + y; // Arrow function for addition
 console.log("Sum of 5 and 10:", sum(5, 10)); // 15
 
+
 let arr = [1, 2, 3, 4, 5, "Hrithicka"];
 // General method
 // arr.filter(function(x) {
@@ -39,8 +40,8 @@ let arr = [1, 2, 3, 4, 5, "Hrithicka"];
 
 // Using Arrow functions
 arr.filter(x => typeof x === "number").map(y => y * 2).forEach(abc => console.log("Element after * 2:", abc * 2));
-console.log(arr.find(x => x === "Hrithicka1")); // undefined
-console.log(arr.find(x => x === "Hrithicka")); // "Hrithicka"
+// console.log(arr.find(x => x === "Hrithicka1")); // undefined
+// console.log(arr.find(x => x === "Hrithicka")); // "Hrithicka"
 
 // Using callback functions
 arr.map(y => y * 2).forEach(abc => console.log("Element after * 2:", abc * 2));
