928025103dc8b795337b77ce399e97717c386540
Hrithicka <hrithigiri03@gmail.com>
2026-05-05 12:27:56 +0530

    setTimeout, setInterval, Promises, API fetching

--- Day4/index.js
+++ Day4/index.js
@@ -1,34 +1,40 @@
+// Asynchronous JavaScript
 // setTimeout function
 setTimeout(() => {
-    console.log("Hello World");
-}, 1000);
+    console.log("Data received");
+}, 5000);
+console.log("test");
 
 // setInterval function
 setInterval(() => {
-    console.log("Hello again");
-}, 2000);
+    console.log("Checking for new data...");
+}, 3000);
 
 // Promises object
-const myDataPromise = new Promise((resolve, reject) => {
-    console.log("Request initiated...");
-    setTimeout(() => {
-        const isSuccess = true; 
-        if (isSuccess) {
-            resolve("Success: Data received! 🚀");
+    function getData() {
+    return new Promise((resolve, reject) => {
+        if(2 > 3) {
+            setTimeout(() => {
+                resolve("Data received");
+            }, 2000);
         } else {
-            reject("Error: Connection timed out. 🔌");
+            reject('not working')
         }
-    }, 2000); // 2-second delay
-});
+    });
+}
 
 // Use .then() to handle success and .catch() to handle errors
-myDataPromise
-    .then((message) => {
-        // Runs if resolve() was called
-        console.log(message); 
-    })
-    .catch((error) => {
-        // Runs if reject() was called
-        console.error(error); 
-    });
-    
+getData().then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.error("Error:", error);
+});
+
+// Fetch API
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
+ 
