ee1274255776b6f019273e5ae5a8524193eb7fa4
Hrithicka <hrithigiri03@gmail.com>
2026-05-05 16:25:13 +0530

    fetching public API done

--- Day4/index.js
+++ Day4/index.js
@@ -34,11 +34,12 @@ getData().then((data) => {
 fetch("https://jsonplaceholder.typicode.com/todos")
     .then(res => res.json()) // res -> string -> json)
     .then(data => data.filter(d => d.id % 2 === 0))
-    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => data.map(d => (d.title = "Hrithicka " + d.title)))
     .then(data => console.log(data))
     .catch(error => console.error("Error fetching data:", error));
  
-// Fetching API with public API
+
+// Fetching API with public API (Today's task)
 // 1) Users API
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
@@ -47,14 +48,12 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then(data => console.log("Users:", data))
   .catch(err => console.error("Error:", err));
 
-
 // 2) Posts API
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(res => res.json())
   .then(data => data.filter(p => p.userId === 1))
   .then(data => data.map(p => p.title))
   .then(data => console.log("Posts:", data));
-
 
 // 3) Products API
 fetch("https://dummyjson.com/products")
@@ -64,14 +63,13 @@ fetch("https://dummyjson.com/products")
   .then(data => data.map(p => p.title))
   .then(data => console.log("Products:", data));
 
-
 // 4) Countries API
-fetch("https://restcountries.com/v3.1/all")
+fetch("https://restcountries.com/v3.1/all?fields=name,population")
   .then(res => res.json())
-  .then(data => data.filter(c => c.population > 100000000))
+  .then(data => data.filter(c => c.population > 10000000))
   .then(data => data.map(c => c.name.common))
-  .then(data => console.log("Countries:", data));
-
+  .then(data => console.log("Countries:", data))
+  .catch(err => console.log("Error:", err));
 
 // 5) Random Users API
 fetch("https://randomuser.me/api/?results=10")
@@ -81,10 +79,10 @@ fetch("https://randomuser.me/api/?results=10")
   .then(data => data.map(u => u.name.first))
   .then(data => console.log("Female Names:", data));
 
-
 // 6) Todos API
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(res => res.json())
   .then(data => data.filter(t => t.completed === true))
   .then(data => data.map(t => t.title))
   .then(data => console.log("Completed Todos:", data));
+
