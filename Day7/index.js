785184d42900e33dc98f816dcf2c4d1936f83724
Hrithicka <hrithigiri03@gmail.com>
2026-05-08 16:32:18 +0530

    hoisting, prototype chaining, array methods

---
+++ Day7/index.js
@@ -0,0 +1,65 @@
+// hoisting
+
+// using let and const --> temporal dead zone
+// console.log(greet);
+// let greet = "Hello, World!";
+// console.log(greet);
+ 
+// using var for hoisting 
+// var greet; --> init -> undefined
+// log()
+// greet = "h W"
+// log()
+
+// function hoisting
+// sayHi();
+// function sayHi() {
+//     console.log('Hello, World!');
+// }
+ 
+// arrow function hoisting
+// var sayHi = () => {
+//     console.log('Hello');
+// }
+
+// prototype
+// Every object --> Object
+const car = {
+    brand: "Tesla",
+    start: () => console.log("VroomVroom")
+}
+// constructor function
+function start() {
+    console.log("VroomVroom")
+}
+const c1 = new start();
+const c2 = new start();
+start.prototype.stop = function() {
+    console.log("Stop");
+}
+// c1.stop();
+// c2.stop();
+
+// car {start} --> prototype {stop}
+// c1 --> start
+// c2 --> start
+// c1.stop();
+
+// prototype chaining
+const animal = { eats: true };
+const rabbit = Object.create(animal); // creates a new object rabbit with prototype animal
+rabbit.carrot = () => console.log("He is eating carrot");
+console.log(rabbit.eats); // inherted from animal
+console.log(rabbit.hasOwnProperty('eats'))
+console.log(rabbit.hasOwnProperty('carrot'))
+console.log(rabbit.toString()) 
+
+// array methods
+// sort() 
+// reverse()
+const arr = [1, 2, 3, -1];
+arr.reverse();
+arr.sort();
+for(let e of arr) { // in -> index, of -> value
+    console.log(e);
+}
