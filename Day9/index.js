efd0067b56dea00e69c5f961be76d11c02a731eb
Hrithicka <hrithigiri03@gmail.com>
2026-05-11 09:35:44 +0530

    advance javascript concepts done

---
+++ Day9/index.js
@@ -0,0 +1,44 @@
+// Advance javascript concepts
+// parameters
+// default parameter
+function welcome(name = "Guest") { // default params
+    console.log(`Welcome ${name}!`);
+}
+
+welcome("Aaryan")
+welcome("Arush")
+welcome()
+
+// spread operator
+const nums = [1, 2, 3, 4, 5]; // spread operator
+const newNums = [...nums, 6, 7, 8];
+console.log(newNums);
+
+// rest operator and parameter
+const obj = {name: "aryan"};
+const updatedObj = {...obj, age: 25};
+console.log(updatedObj);
+
+const cal = { // rest operator
+    sum: (...nums) => nums.reduce((a, b) => a + b, 0), // rest parameters
+}
+
+console.log(cal.sum(1, 2, 43, 4))
+
+// destructuring 
+// general way
+const arr = ["Aryan", "Arush", "Vivek", "Rohit"];
+const first = arr[0];
+const second = arr[1];
+const third = arr[2];
+const fourth = arr[3];
+
+// destructured way (position-based extraction)
+const [first, second, third, fourth, fifth] = ["Aryan", "Arush", "Vivek", "Rohit"]; // pos-based extraction
+const [one, two, ...three] = [1, 3, 5, 6, 7];
+console.log(first, second, third, fourth, fifth);
+console.log(one, two, three)
+
+// (key-based extraction)
+const { name, age } = {name: "aryan", age: 25}; // key-based extraction
+console.log(name, age);
