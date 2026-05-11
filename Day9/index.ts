85ff262e0379ab78bad6d9b895a1583134baf9e5
Hrithicka <hrithigiri03@gmail.com>
2026-05-11 13:08:10 +0530

    typescript basics

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,2 +1,16 @@
-let name = "Hrithicka";
-console.log(name);
+// Basics of typescript
+
+// let name = "Hrithicka";
+// console.log(name);
+// output : Hrithicka
+
+let name: string = "Hrithicka";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+console.log(add(5, 10));
